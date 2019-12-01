f21ad27bdd537a283db6411d4b8fcb5db6a668cd
Nicolas Martyanoff <khaelin@gmail.com>
2019-12-01 13:06:17 +0100

    initial import

---
+++ aoc2019.asd
@@ -0,0 +1,9 @@
+
+(defsystem "aoc2019"
+    :description "Advent of Code 2019"
+    :author "Nicolas Martyanoff <khaelin@gmail.com>"
+    :licence "ISC"
+    :depends-on ("cl-ppcre")
+    :pathname "src"
+    :serial t
+    :components ((:file "aoc")))
---
+++ src/aoc.lisp
@@ -0,0 +1,31 @@
+
+(defpackage :aoc2019
+  (:use :cl)
+  (:export :read-file
+           :do-file-lines
+           :read-file-lines))
+
+(in-package :aoc2019)
+
+(defun read-file (path)
+  (with-open-file (file path :element-type 'character
+                             :external-format :utf-8)
+    (let ((data (make-array (file-length file) :element-type 'character)))
+      (read-sequence data file)
+      data)))
+
+(defmacro do-file-lines ((path line &optional result-var) &body body)
+  (let ((stream (gensym "STREAM-")))
+    `(with-open-file (,stream ,path)
+       (handler-case
+           (loop
+             (let ((,line (read-line ,stream)))
+               (progn
+                 ,@body)))
+         (end-of-file ()
+           ,result-var)))))
+
+(defun read-file-lines (path &optional (transform #'identity))
+  (let ((lines nil))
+    (do-file-lines (path line (nreverse lines))
+      (push (funcall transform line) lines))))
