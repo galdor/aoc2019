e55ce98db025850b5fe8bd7c801b85e8f39c2508
Nicolas Martyanoff <khaelin@gmail.com>
2019-12-05 08:54:24 +0100

    prepare problem 5

---
+++ src/05.lisp
@@ -0,0 +1,17 @@
+
+(defpackage :aoc2019-05
+  (:use :cl :aoc2019-utils)
+  (:export :run1 :run2 :check))
+
+(in-package :aoc2019-05)
+
+;;; https://adventofcode.com/2019/day/5
+
+(defun run1 ()
+  nil)
+
+(defun run2 ()
+  nil)
+
+(defun check ()
+  (check-problem-results 5 nil nil))
